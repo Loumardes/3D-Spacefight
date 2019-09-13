ac992b2f92cccb0d3b4c8cede2f0f586ce96e967
Loumardes <Loumardes@gmail.com>
2019-09-13 13:49:38 +0200

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // display size
+    private float sHeight;
+    private float sWidth;
+
+    // mouse pos
+    private float mHeight;
+    private float mWidth;
+
+    // ship stats
+    public bool is_drived;
+
+    //public Sstats shipstats = new Sstats();
+    //private Sstats sstats;
+
+    public float shipSpeed;
+ //   shipSpeed = sstats.ShipSpeed;
+    public float acceleration = 0.1f;
+
+    public float pitch = 2f;
+    public float yaw = 2f;
+    public float roll = 2f;
+
+
+    private Vector2 mousePos;
+
+
+    public float ShipSpeed;
+    private float moveSpeed;
+    private Vector2 direction;
+    private Vector3 movement;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sHeight = Screen.height / 2;
+        sWidth = Screen.width / 2;
+
+
+        moveSpeed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (is_drived)
+        {
+            // Throttle Control
+            if (Input.GetKey("z") & moveSpeed <= ShipSpeed)
+            {
+                moveSpeed += acceleration;
+            }
+
+            if (Input.GetKey("s") & moveSpeed >= 0f)
+            {
+                moveSpeed -= acceleration;
+            }
+        }
+
+        //moving forward
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (is_drived)
+        {
+            direction = Input.mousePosition;
+            mHeight = direction.y - sHeight;
+            mWidth = direction.x - sWidth;
+
+            transform.Rotate(new Vector3(0f, mWidth * pitch * Time.deltaTime, 0f));
+            transform.Rotate(new Vector3(-mHeight * yaw * Time.deltaTime, 0f, 0f));
+            // Direction Control
+            if (Input.GetKey("q"))
+            {
+                transform.Rotate(new Vector3(0f, 0f, 50 * roll * Time.deltaTime));
+            }
+
+            if (Input.GetKey("d"))
+            {
+                transform.Rotate(new Vector3(0f, 0f, -50 * roll * Time.deltaTime));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Universal_movement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Universal_movement : MonoBehaviour
+{
+    // display size
+    private float Screen_Height;
+    private float Screen_Width;
+
+    // mouse pos
+    public float Sensitivity = 100;
+    private float mHeight;
+    private float mWidth;
+
+    private Vector2 direction;
+
+    // keyboard input
+    float key_Horizontal;
+    float key_Vertical;
+
+    // ship stats
+    public Rigidbody Body;
+
+    public bool is_drived;
+
+    public float pitch = 2f;
+    public float yaw = 2f;
+    public float roll = 2f;
+
+    public float Max_Thrust;
+    public float Thrust;
+    public float Thrust_change;
+    public float Space_Drag;
+
+    void Start()
+    {
+        Screen_Height = Screen.height / 2;
+        Screen_Width = Screen.width / 2;
+
+
+        Thrust = 0f;
+    }
+    void FixedUpdate()	// Update à chaque frame physique
+    {
+        if (is_drived)
+        {
+            // Throttle Control
+            if (Input.GetKey("z") & Thrust <= Max_Thrust)
+            {
+                Thrust += Thrust_change;
+            }
+
+            if (Input.GetKey("s") & Thrust >= 0f)
+            {
+                Thrust -= Thrust_change;
+            }
+            Thrust = Mathf.Clamp(Thrust, 0f, Max_Thrust);
+
+            // Direction Control
+            direction = Input.mousePosition;
+            mHeight = Mathf.Clamp(direction.y - Screen_Height, -Sensitivity, Sensitivity) / Sensitivity;
+            mWidth = Mathf.Clamp(direction.x - Screen_Width, -Sensitivity, Sensitivity) / Sensitivity;
+
+            transform.Rotate(new Vector3(0f, mWidth * pitch * Time.deltaTime, 0f));
+            transform.Rotate(new Vector3(-mHeight * yaw * Time.deltaTime, 0f, 0f));
+            // Direction Control
+            if (Input.GetKey("q"))
+            {
+                transform.Rotate(new Vector3(0f, 0f, roll * Time.deltaTime));
+            }
+
+            if (Input.GetKey("d"))
+            {
+                transform.Rotate(new Vector3(0f, 0f, -roll * Time.deltaTime));
+            }
+        }
+
+
+        //Vector3 movement = new Vector3(mHorizontal, 0.0f, mVertical);
+
+        Body.AddRelativeForce(0, 0, Thrust, ForceMode.Force);
+
+    }
+}
